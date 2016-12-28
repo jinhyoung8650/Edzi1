801095a1b2f431b987aa31a7070fd30ff85a4102
jinhyoung <jinhyoung8650@daum.net>
2016-12-28 22:55:42 +0900

    remocon_2

--- project2/Edzi1-patch-1/js/remote.js
+++ project2/Edzi1-patch-1/js/remote.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
-   
-    $("html").append("<div id='container' style='position: fixed;  width: 60px; height: 110px; border: 3px solid black; top: 120px; left:94.7%'>");
+
+if (matchMedia("screen and (min-width: 1024px)").matches) {
+ $("html").append("<div id='container' style='position: fixed;  width: 60px; height: 110px; border: 3px solid black; top: 120px; left:94.7%'>");
     var  container= $("#container").draggable();
+    
     
     var remocon = new frame.animation({
          url : "img/remocon.png",
@@ -14,11 +16,11 @@ $(document).ready(function(){
     });
     
     
-    remoconImg  = frame.addSprite(container,"remoconImg",{width: 44, height:                                                36,x:05,y:0});
+    remoconImg  = frame.addSprite(container,"remoconImg",{width: 44, height:                                                36,x:3.5,y:0});
     top_ButtonImg  = frame.addSprite(container,"top_ButtonImg",{width: 17, height:                                          27,x:8.5,y:76.8});
     btm_ButtonImg  = frame.addSprite(container,"btm_ButtonImg",{width: 17, height:                                          27,x:38.5,y:76.8});
     currentNumber = frame.addSprite(container,"currentNumber",                                                              {width:20,height:40,x:10,y:34});
-    allNumber  = frame.addSprite(container,"allNumber",{width: 20, height:                                                  40,x:34.5,y:34});
+    allNumber  = frame.addSprite(container,"allNumber",{width: 20, height:                                                  40,x:37.5,y:34});
    
     frame.setAnimation(remoconImg ,remocon);
     frame.setAnimation(top_ButtonImg ,top_Button);
@@ -29,48 +31,45 @@ $(document).ready(function(){
     
     scroll_height=$(document).scrollTop();
     
+    /*remocon*/
+    var sec_position=$(".sec");
+    
     top_ButtonImg.off("click").on("click",function(){
-            var top_position= new Array();
-            var sec_position=$(".sec");
-            sec_position.each(function(index,data){
-            scroll_height=$(document).scrollTop();
-            top_position[index]=$("#sec"+(index+1)).offset().top;
-            if(scroll_height>=top_position[index]){
-                  console.log(scroll_height+"  /   "+top_position[index]);
-              $("html,body").stop().animate({scrollTop:top_position[index]-150}
-                ,1000,"easeOutCubic");
-                $("#log").text(index+1);
+          scroll_height=$(document).scrollTop();
+           sec_position.each(function(index,data){
+            var pos = $(this).offset().top;
+               if(scroll_height > pos){
+                $("html,body").stop().animate({scrollTop:pos},1000);
+                 $("#log").text(index+1);
             }
-          });
+          })
         });
     btm_ButtonImg.off("click").on("click",function(){
-            var top_position= new Array();
-            var sec_position=$(".sec");
-            sec_position.each(function(index,data){
-            scroll_height=$(document).scrollTop();
-            top_position[index]=$("#sec"+(index+1)).offset().top;
-           /* if(scroll_height<=top_position[index]){
-              $("html,body").stop().animate(
-                {scrollTop:top_position[index]}
-                ,1000,"easeOutCubic");
-            }*/
-        });
-             if(scroll_height<=top_position[0]){
-                   $("html,body").stop().animate(
-                {scrollTop:top_position[1]-150}
-                ,1000,"easeOutCubic");
-                 $("#log").text(2);
-              }else if(scroll_height<=top_position[1]){
-                      $("html,body").stop().animate(
-                {scrollTop:top_position[2]-150}
-                ,1000,"easeOutCubic");
-                   $("#log").text(3);
-              } else if(scroll_height<=top_position[2]){
-                      $("html,body").stop().animate(
-                {scrollTop:top_position[2]-150}
-                ,1000,"easeOutCubic");
-                   $("#log").text(3);
-              }   
+         scroll_height=$(document).scrollTop();
+          sec_position.each(function(index,data) {
+           var pos = $(this).offset().top;  
+               if (scroll_height < pos) {      
+                $('html, body').stop().animate({scrollTop: pos},1000);  
+                 $("#log").text(index+1);
+                  return false;
+          }
+       });
     });
+    top_ButtonImg.on("hover",function(){
+        top_ButtonImg.css({border: '1px solid rgba(122,252,235,0.8)',borderRadius: '50%'});
+    }).on("mouseleave",function(){
+        top_ButtonImg.css({border: '0px',borderRadius: '50%'});
+    });
+     btm_ButtonImg.on("hover",function(){
+        btm_ButtonImg.css({border: '1px solid rgba(122,252,235,0.8)',borderRadius: '50%'});
+    }).on("mouseleave",function(){
+        btm_ButtonImg.css({border: '0px',borderRadius: '50%'});
+    });
+    
+} else {
+  // 1024px 미만에서 사용할 JavaScript
+}
+           
      
-});
+   });
+   
