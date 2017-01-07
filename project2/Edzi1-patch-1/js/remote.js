66397ed460f7072c9c12587d3980398689ce8c38
jinhyoung <jinhyoung8650@daum.net>
2017-01-07 15:22:45 +0900

    v0.17 remocon

--- project2/Edzi1-patch-1/js/remote.js
+++ project2/Edzi1-patch-1/js/remote.js
@@ -16,31 +16,29 @@ var remocon =function(){
     
     
     remoconImg  = frame.addSprite(container,"remoconImg",{width: 44, height:                                                36,x:3.5,y:0});
-    top_ButtonImg  = frame.addSprite(container,"top_ButtonImg",{width: 20, height:                                          27,x:5.5,y:76.8});
-    btm_ButtonImg  = frame.addSprite(container,"btm_ButtonImg",{width: 20, height:                                          27,x:35.5,y:76.8});
+    top_ButtonImg  = frame.addSprite(container,"top_ButtonImg",{width: 18.5, height:                                          27,x:5.5,y:76.8});
+    btm_ButtonImg  = frame.addSprite(container,"btm_ButtonImg",{width: 16.5, height:                                          27,x:37.5,y:76.8});
     currentNumber = frame.addSprite(container,"currentNumber",                                                              {width:20,height:40,x:8.1,y:34});
     allNumber  = frame.addSprite(container,"allNumber",{width: 20, height:                                                  40,x:35.5,y:34});
    
     frame.setAnimation(remoconImg ,remocon);
     frame.setAnimation(top_ButtonImg ,top_Button);
     frame.setAnimation(btm_ButtonImg ,btm_Button);
-    
-    currentNumber.append("<div id='log' style='font-size: 1.8em; left:10%'>1</div>");
-    allNumber.append("<div style='font-size: 1.9em; left:10%'>4</div>");
+    scroll_height=$(document).scrollTop();
+    currentNumber.append("<div id='log' style='font-size: 1.8em; left:10%'></div>");
+    allNumber.append("<div  style='font-size: 1.9em; left:10%'>"+$(".sec").length+"+</div>");
     
     scroll_height=$(document).scrollTop();
-    
-    /*remocon*/
     var sec_position=$(".sec");
-    
+    /*remocon*/
+  
     top_ButtonImg.off("click").on("click",function(){
           scroll_height=$(document).scrollTop();
-           sec_position.each(function(index,data){
-            var pos = $(this).offset().top;
+         sec_position.each(function(index,data){
+                var pos = $(this).offset().top;
+               console.log(pos);
                if(scroll_height > pos+1){
                 $("html,body").stop().animate({scrollTop:pos},1000);
-                 $("#log").text(index+1);
-                
             }
           })
         });
@@ -50,7 +48,6 @@ var remocon =function(){
            var pos = $(this).offset().top;  
                if (scroll_height < (pos-1)) {      
                 $('html, body').stop().animate({scrollTop: pos},1000);  
-                 $("#log").text(index+1);
                   return false;
           }
        });
@@ -66,13 +63,31 @@ var remocon =function(){
         btm_ButtonImg.css({border: '0px',borderRadius: '50%'});
     });
 }
-
+function currentScroll(){
+       var sec_position=$(".sec");
+        
+    scroll_height=$(document).scrollTop();
+       var firstSec=$(".sec:eq(0)").offset().top; 
+         sec_position.each(function(index,data){
+          var pos = $(this).offset().top*0.9;
+           if(scroll_height+firstSec > pos){
+              $("#log").text(index+1);
+            }
+          })
+   }
 /*1024d이상 */
 if (matchMedia("screen and (min-width: 1024px)").matches) {
+     
+    
     remocon();
+    currentScroll();
+    
+    $(window).scroll(function(event){
+          currentScroll(); 
+    });
 }
 else{
-    
+  
 }
     /*화면 움직일때 1024이상이*/
      $(window).resize(function() {
@@ -87,10 +102,13 @@ else{
         var contain = $("#container");
       var device_width=$(window).width();
         if(device_width>1025){
+      
             if ( $("#container").length > 0 ) {
                 $("#container").show();
             } else {
+                
                 remocon();
+                currentScroll();
             }
         }else{
             $("#container").hide();
